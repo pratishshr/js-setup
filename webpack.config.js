de08674a522a227ebde947f9316616178d7bbd0b
pratishshr <pratishshr@gmail.com>
2018-12-13 12:34:54 +0545

    Update babel to v7

--- webpack.config.js
+++ webpack.config.js
@@ -7,5 +7,14 @@ module.exports = {
     filename: 'lib.js',
     library: 'lib',
     libraryTarget: 'umd'
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, use: ['babel-loader'] },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
   }
 };
